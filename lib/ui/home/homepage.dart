ea73d5d358b9c7b0ccdd5a96bd2d97686b31992e
Strange-Philip <philipabakahmensah@gmail.com.com>
2023-10-25 12:12:36 +0000

    added notifcation sheet

--- lib/ui/home/homepage.dart
+++ lib/ui/home/homepage.dart
@@ -13,7 +13,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppbar());
+    return Scaffold(
+      appBar: buildAppbar(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              // padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(color: DocumensoColors.neutral.shade200)),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/sheets_plus.png",
+                      height: 90,
+                      width: 120,
+                    ),
+                    Text("Add document",
+                        style: DocumensoTypography()
+                            .body1SemiBold
+                            .copyWith(color: DocumensoColors.neutral.shade400)),
+                    Text("Tap here to upload your document.",
+                        style: DocumensoTypography()
+                            .body2SemiBold
+                            .copyWith(color: DocumensoColors.neutral.shade300)),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   AppBar buildAppbar() {
@@ -42,14 +79,17 @@ class _HomePageState extends State<HomePage> {
       centerTitle: false,
       backgroundColor: DocumensoColors.white,
       actions: [
-        CircleAvatar(
-          radius: 15,
-          backgroundColor: DocumensoColors.primary.shade400,
-          child: Text("1",
-              textAlign: TextAlign.center,
-              style: DocumensoTypography()
-                  .captionMedium
-                  .copyWith(color: DocumensoColors.neutral.shade800)),
+        GestureDetector(
+          onTap: () => showNotificationBtnSheet(context),
+          child: CircleAvatar(
+            radius: 15,
+            backgroundColor: DocumensoColors.primary.shade400,
+            child: Text("1",
+                textAlign: TextAlign.center,
+                style: DocumensoTypography()
+                    .captionMedium
+                    .copyWith(color: DocumensoColors.neutral.shade800)),
+          ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -145,6 +185,109 @@ class _HomePageState extends State<HomePage> {
                       thickness: 1,
                     ),
                     buildtabs(name: "Logout", hasArrow: false, icon: "assets/icons/log-out.svg"),
+                  ],
+                ),
+              ),
+            ));
+  }
+
+  void showNotificationBtnSheet(BuildContext context) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        isDismissible: true,
+        enableDrag: true,
+        context: context,
+        builder: (context) => ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 600,
+              ),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 13),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: DocumensoColors.white, borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Notifications",
+                            textAlign: TextAlign.center,
+                            style: DocumensoTypography()
+                                .title
+                                .copyWith(color: DocumensoColors.neutral.shade800)),
+                        const Spacer(),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: SvgPicture.asset("assets/icons/close.svg",
+                                color: DocumensoColors.black)),
+                      ],
+                    ),
+                    Divider(
+                      color: DocumensoColors.neutral.shade200,
+                      thickness: 1,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                  radius: 16,
+                                  backgroundColor: DocumensoColors.primary.shade200,
+                                  child: Text("TK",
+                                      textAlign: TextAlign.center,
+                                      style: DocumensoTypography()
+                                          .body2Medium
+                                          .copyWith(color: DocumensoColors.primary.shade600))),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                        text: "Timur Ercan",
+                                        style: DocumensoTypography().body2SemiBold.copyWith(
+                                            color: DocumensoColors.neutral.shade800, height: 1.4)),
+                                    TextSpan(
+                                        text: " has invited you to\nsign",
+                                        style: DocumensoTypography().body2Medium.copyWith(
+                                            color: DocumensoColors.neutral.shade400, height: 1.4)),
+                                    TextSpan(
+                                        text: " Document.pdf",
+                                        style: DocumensoTypography().body2Medium.copyWith(
+                                            color: DocumensoColors.neutral.shade800, height: 1.4)),
+                                    TextSpan(
+                                        text: " - 2h ago",
+                                        style: DocumensoTypography().body2Medium.copyWith(
+                                            color: DocumensoColors.neutral.shade400, height: 1.4)),
+                                  ],
+                                ),
+                              ),
+                              const Spacer(),
+                              SvgPicture.asset("assets/icons/check.svg",
+                                  height: 20, width: 20, color: DocumensoColors.neutral.shade400),
+                            ],
+                          ),
+                        ),
+                        PreferredSize(
+                          preferredSize: const Size.fromHeight(1),
+                          child: Container(
+                            color: DocumensoColors.neutral.shade100,
+                            height: 1,
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
